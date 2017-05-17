07577cef102a2217ad576558f97c548d8cc5357d
Hillis, Andrew <ahillis@hbs.edu>
2017-05-16 20:31:26 -0400

    Add POST /todos route and refactor mongoose

--- server/server.js
+++ server/server.js
@@ -1,42 +1,47 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser= require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp'); 
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-// Set Up Schema 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String
-  },
-  completed: {
-    type: Boolean
-  },
-  completedAt: {
-    type: Number
-  }
+var app = express();
+
+//  allows us to receive json 
+app.use(bodyParser.json()); 
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
+
+
+app.listen(3000, () =>{
+  console.log('Started on port 3000');
+});
+
+
 
 // Create Instance and Insert Document 
+// var newTodo2 = new Todo({
+//   text: ' lets see' 
+// });
 
-var newTodo = new Todo({
-  text: 'Cook dinner'
-});
 
-newTodo.save().then((doc) => {
-  console.log('saved todo', doc);
-}, (e) => {
-  console.log(e);
-});
 
-var newTodo2 = new Todo({
-  text: 'Cook dinner',
-  completed: true,
-  completedAt: 121
-});
+// var newUser = new User({
+//   email: 'hillis18@gmail.com'
+// });
 
-newTodo2.save().then((doc) => {
-  console.log('saved todo', doc);
-}, (e) => {
-  console.log(e);
-});
-
+// newUser.save().then((doc) => {
+//   console.log('saved user', doc);
+// }, (e) => {
+//   console.log(e);
+// });
