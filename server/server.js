11404b8ee82e9a0331a6943cf806497554af8984
Hillis, Andrew <ahillis@hbs.edu>
2017-05-19 19:44:03 -0400

    Add find by id route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var bodyParser= require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
 
 var app = express();
 
@@ -23,7 +25,27 @@ app.post('/todos', (req, res) => {
   });
 });
 
-// 
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    
+    res.send({todo});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+
+// get all todos 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
